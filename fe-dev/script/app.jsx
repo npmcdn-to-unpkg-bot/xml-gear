fc7266e16ec9029c0c81598074ad72654bba92c6
JasonApeman <jasonapeman@outlook.com>
2016-03-10 18:12:12 +0800

    checker

--- fe-dev/script/app.jsx
+++ fe-dev/script/app.jsx
@@ -10,15 +10,61 @@ import { Router, Route, IndexRoute, Redirect, Link, browserHistory } from 'react
 //reflux
 var Actions=require('./action/action.js');
 
+//libs
+var parser = require('xml-parser');
+var xmlChecker = require('xmlchecker');
+
 //App
 var App = React.createClass({
 
 	mixins: [Reflux.ListenerMixin],
 
+	getDefaultProps: function(){
+			return {};
+	},
+
+	getInitialState: function(){
+			return {};
+	},
+
+	componentDidMount: function(){
+
+			this.codeMirror=CodeMirror(document.getElementById("editArea"));
+
+	},
+
+	parseXML: function(){
+
+		var str=this.codeMirror.getValue();
+
+		try{
+			xmlChecker.check(str);
+		}
+		catch(error){
+			alert("XML Parser: " + error.name + " at " + error.line + "," + error.column + ": " + error.message);
+
+			return;
+		}
+
+		var obj = parser(str);
+
+		console.log(obj);
+	},
+
 	render: function(){
 		return(
 			<div>
-				APP
+				<nav className="navbar navbar-default navbar-fixed-top">
+					<div className="container-fluid">
+						<div className="navbar-header">
+							<a className="navbar-brand" href="#">XML Gear</a>
+						</div>
+						<button type="button" className="btn btn-default navbar-btn" onClick={this.parseXML}>Check</button>
+					</div>
+				</nav>
+				<div className="content_container">
+					<div className="editor_editArea" id="editArea"></div>
+				</div>
 			</div>
 			)
 	}
